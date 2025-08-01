1be61393dfd2ebc4b16abc793d938ab111be564f
aacyy <acinonyx.acy@gmail.com>
2025-08-01 12:20:33 +0200

    Update SectionModels.jsx

--- app/components/SectionModels.jsx
+++ app/components/SectionModels.jsx
@@ -108,6 +108,23 @@ const SectionModels = () => {
 			descriptionDown:
 				'Širina košnje je 55 cm, kar omogoča učinkovito košnjo površin do 2000 m².Kljub manjšim dimenzijam se lahko pohvali z zanesljivim 224 ccm3 bencinskim motorjem, ki razvije 9 KM (6,62 kW) moči, kar je več kot dovolj za učinkovito mulčenje srednje visoke trave. Kosilnica je opremljena z alternatorjem za samodejno polnjenje baterije med delovanjem. Njena poraba je izredno nizka in znaša približno 0,8 litra na uro.',
 		},
+		{
+			modelName: 'Mulčer KM-55',
+			modelType: 'Goseničar',
+			price: '3,290',
+			motor: 'Bencinski motor - 9 KM',
+			domet: 'Do 200m',
+			kosnja: 'Daljinsko nastavljiva višina',
+			povrsina: 'Do 1500 m2 v eni uri',
+			dimenzije: '920 x 1030 x 630',
+			maxKosnja: 'Maksimalen naklon 40°',
+			teza: '133kg',
+			image: '/images/mulčer_4x4.jpg',
+			descriptionUp:
+				'Daljinsko vodena mulčerska kosilnica na kolesa združuje vse ključne karakteristike modela KM 55 na gosenice. Razlika je v podvozju, namesto gosenic ima ta model štirikolesni pogon (4x4), ki omogoča okretnost, večjo hitrost na ravnini in odličen oprijem tudi na bolj razgibanih terenih. ',
+			descriptionDown:
+				'Širina košnje je 55 cm, kar omogoča učinkovito košnjo površin do 2000 m².Kljub manjšim dimenzijam se lahko pohvali z zanesljivim 224 ccm3 bencinskim motorjem, ki razvije 9 KM (6,62 kW) moči, kar je več kot dovolj za učinkovito mulčenje srednje visoke trave. Kosilnica je opremljena z alternatorjem za samodejno polnjenje baterije med delovanjem. Njena poraba je izredno nizka in znaša približno 0,8 litra na uro.',
+		},
 	];
 	const [filteredData, setFilteredData] = useState({
 		modelName: 'Mulčer KM-55',
@@ -263,6 +280,69 @@ const SectionModels = () => {
 						<h4>( cena z ddv )</h4>
 					</div>
 				</div>
+				{(model === 0 || model === 1) && (
+	<div className='sb-section-models-box'>
+		<div className='sb-section-models-item'>
+			<div className='sb-section-models-item-image'>
+				<div className='sb-section-models-item-title'>
+					<h1>{sbModels[6].modelName}</h1>
+					<h1>{sbModels[6].modelType}</h1>
+				</div>
+				<div
+					className='sb-section-models-item-image-image'
+					style={{
+						backgroundImage: sbModels
+							? `url(${sbModels[6].image})`
+							: `url(${imgMain})`,
+					}}
+				></div>
+			</div>
+			<div className='sb-section-models-item-table'>
+				<div className='sb-section-models-item-table-title'>
+					<h1>Specifikacije modela</h1>
+				</div>
+				<table>
+					<tbody>
+						<tr className='tr-grey'>
+							<td className='td-title'>Motor</td>
+							<td>{sbModels[6].motor}</td>
+						</tr>
+						<tr className='tr-white'>
+							<td className='td-title'>Domet</td>
+							<td>{sbModels[6].domet}</td>
+						</tr>
+						<tr className='tr-grey'>
+							<td className='td-title'>Košnja</td>
+							<td>{sbModels[6].kosnja}</td>
+						</tr>
+						<tr className='tr-white'>
+							<td className='td-title'>Površina</td>
+							<td>{sbModels[6].povrsina}</td>
+						</tr>
+						<tr className='tr-grey'>
+							<td className='td-title'>Dimenzije (mm)</td>
+							<td>{sbModels[6].dimenzije} (D X Š X V )</td>
+						</tr>
+						<tr className='tr-white'>
+							<td className='td-title'>Naklon</td>
+							<td>{sbModels[6].maxKosnja}</td>
+						</tr>
+						<tr className='tr-grey'>
+							<td className='td-title'>Teža</td>
+							<td>{sbModels[6].teza}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div className='sb-models-price-box'>
+			<div className='sb-models-price'>
+				<h3>{sbModels[6].price} &#8364;</h3>
+				<h4>( cena z ddv )</h4>
+			</div>
+		</div>
+	</div>
+)}
 			</div>
 			<div className='sb-section-models-description'>
 				<div className='sb-section-models-description-up'>
